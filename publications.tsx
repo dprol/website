2e96dff26a90d3ff85da66077b612b65383ac450
Danny Prol <daniel.prol@glovoapp.com>
2026-08-26 14:24:02 +0000

    modified:   publications.tsx

--- publications.tsx
+++ publications.tsx
@@ -1,34 +1,5 @@
 import { JSX } from "preact";
 
-interface Author {
-  name: string;
-  href: string;
-}
-
-const author = (name: string, href: string): Author => ({ name, href });
-
-// Authors from the publications
-const danielProl = author("Daniel Prol", "/");
-const aminAlipour = author("Amin Alipour", "https://www.aminalipour.com/");
-const jamessPrather = author("James Prather", "https://jamesprather.com/");
-const paulDenny = author("Paul Denny", "https://www.cs.auckland.ac.nz/~paul/");
-const juhoLeinonen = author("Juho Leinonen", "https://juholeinonen.com/");
-const aayush = author("Aayush Kumar", "https://www.linkedin.com/in/aayush-kumar-05a990213/");
-const sruti = author("Sruti Srinivasa Ragavan", "https://sruti-s-ragavan.github.io/");
-const dayeNam = author("Daye Nam", "https://dayenam.com/");
-const michaelHilton = author("Michael Hilton", "https://www.cs.cmu.edu/~mhilton/");
-const aliAlfageeh = author("Ali Alfageeh", "https://www.linkedin.com/in/ali-alfageeh-182755132/");
-const sadeghAlMahdi = author("Mahdi Kazemi", "https://www.linkedin.com/in/sadegh-almahdi-kazemi/");
-const matinAmoozadeh = author("Matin Amoozadeh", "https://www.linkedin.com/in/matin-amoozadeh-87b8231b3/");
-const soutiChattopadhyay = author("Souti Chattopadhyay", "https://viterbi.usc.edu/directory/faculty/Chattopadhyay/Souti");
-
-// ITiCSE 2026 WG10 leaders
-// TODO: completar la lista de miembros del WG y sus enlaces desde la pagina de ACM
-const peterOhmann = author("Peter Ohmann", "");
-const edNovak = author("Ed Novak", "");
-const scottReckinger = author("Scott Reckinger", "");
-const shanonReckinger = author("Shanon Reckinger", "https://dl.acm.org/profile/99658741464");
-
 interface Venue {
   name: string;
   href: string;
@@ -38,7 +9,6 @@ interface Publication {
   title: string;
   href: string;
   venue: Venue;
-  authors: Author[];
   year: number;
   type: string;
 }
@@ -51,8 +21,6 @@ const pubs: Publication[] = [
       name: "ITiCSE 2026",
       href: "https://iticse.acm.org/2026/",
     },
-    // TODO: anadir el resto de miembros del WG10 en el orden de la publicacion
-    authors: [peterOhmann, edNovak, scottReckinger, shanonReckinger, danielProl],
     year: 2026,
     type: "Abstract",
   },
@@ -63,7 +31,6 @@ const pubs: Publication[] = [
       name: "Re-UNIR",
       href: "https://reunir.unir.net/",
     },
-    authors: [danielProl],
     year: 2025,
     type: "Thesis",
   },
@@ -74,7 +41,6 @@ const pubs: Publication[] = [
       name: "arXiv Preprint",
       href: "https://arxiv.org/",
     },
-    authors: [aayush, danielProl, aminAlipour, sruti],
     year: 2025,
     type: "Preprint",
   },
@@ -85,11 +51,6 @@ const pubs: Publication[] = [
       name: "Koli Calling 2025",
       href: "https://www.kolicalling.fi/",
     },
-    authors: [
-      aliAlfageeh, sadeghAlMahdi, dayeNam, danielProl, matinAmoozadeh,
-      soutiChattopadhyay, jamessPrather, paulDenny, juhoLeinonen,
-      michaelHilton, sruti, aminAlipour
-    ],
     year: 2025,
     type: "Conference",
   },
@@ -100,37 +61,10 @@ const pubs: Publication[] = [
       name: "Koli Calling 2024",
       href: "https://www.kolicalling.fi/",
     },
-    authors: [
-      matinAmoozadeh, dayeNam, danielProl, aliAlfageeh, jamessPrather,
-      michaelHilton, sruti, aminAlipour
-    ],
     year: 2024,
     type: "Conference",
   },
 ];
-
-const authors = (array: Author[]): JSX.Element => {
-  const elems = array.map(({ name, href }) => <a href={href}>{name}</a>);
-  if (elems.length === 0) {
-    throw Error("no authors");
-  } else if (elems.length === 1) {
-    return elems[0];
-  } else if (elems.length === 2) {
-    return (
-      <>
-        {elems[0]} and {elems[1]}
-      </>
-    );
-  } else {
-    const last = elems.pop();
-    const commas = elems.map((elem) => <>{elem}, </>);
-    return (
-      <>
-        {commas}and {last}
-      </>
-    );
-  }
-};
 
 export const publications = (): JSX.Element => (
   <div class="publications">
@@ -141,7 +75,6 @@ export const publications = (): JSX.Element => (
             <a href={pub.href}>{pub.title}</a>
           </div>
           <div class="pub-info">
-            {authors(pub.authors)}.{" "}
             <a class="venue" href={pub.venue.href}>
               {pub.venue.name}
             </a>{" "}
